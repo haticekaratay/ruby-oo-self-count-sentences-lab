11c9010fae25a2dfd9a9df0daeac8b0d5182c269
Hatice <haticekaraty@gmail.com>
2021-01-29 00:02:22 -0500

    commit tree a05200e9706af72aebc5ee95d92b43f7d6766c10

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    word_count = 0
+    self_array = self.split(/([?!.])/)
+    self_array.each{|el|
+      if el.length>1
+        word_count += 1
+      else
+        word_count = word_count
+      end
+    }
+    
+    word_count
   end
 end
+puts  " "
